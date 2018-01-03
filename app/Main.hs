8f844aef4e090ab298d7a08d4af30154c993a91d
Keith <hnefatl@gmail.com>
2018-01-03 12:41:43 +0000

    Added demo executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
+import System.Environment (getArgs)
 import DFA
 
+
 main :: IO ()
-main = undefined
+main = do
+    args <- getArgs
+    let arglength = length args
+    
+    if (arglength == 1) && (args !! 0 `notElem` helpCommands) then
+        case toInputs $ args !! 0 of
+            Nothing -> printHelp
+            Just i  -> do let result = evaluate i
+                              s = sum result
+                          putStrLn $ (show result)
+                          putStrLn $ "Sum:    " ++ (show s)
+    else
+        printHelp
+
+
+helpCommands :: [String]
+helpCommands = ["help", "--help", "-h", "--h", "/h", "/?", "/help"]
+
+printHelp :: IO ()
+printHelp = do
+    putStrLn "Usage: DFA <input>"
+    putStrLn "<input> should be a string of characters from \"+-ne\":"
